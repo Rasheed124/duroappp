997fffb5ffb894f424e1083d26511b26becc45c1
Rasheed124 <rashdevatrealglobe@gmail.com>
2023-11-14 06:29:32 -1100

    new portfolio blog

--- server.ts
+++ server.ts
@@ -10,31 +10,31 @@ const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
  
 app.prepare().then(() => {
-  createServer(async (req: { url: any }, res: { statusCode: number; end: (arg0: string) => void }) => {
-    try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
-      const parsedUrl = parse(req.url, true)
-      const { pathname, query } = parsedUrl
- 
-      if (pathname === '/a') {
-        await app.render(req, res, '/a', query)
-      } else if (pathname === '/b') {
-        await app.render(req, res, '/b', query)
-      } else {
-        await handle(req, res, parsedUrl)
-      }
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err)
-      res.statusCode = 500
-      res.end('internal server error')
-    }
-  })
-    .once('error', (err: any) => {
-      console.error(err)
-      process.exit(1)
-    })
-    .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`)
-    })
+ createServer(async (req, res) => {
+   try {
+     // Be sure to pass `true` as the second argument to `url.parse`.
+     // This tells it to parse the query portion of the URL.
+     const parsedUrl = parse(req.url, true);
+     const { pathname, query } = parsedUrl;
+
+     if (pathname === "/a") {
+       await app.render(req, res, "/a", query);
+     } else if (pathname === "/b") {
+       await app.render(req, res, "/b", query);
+     } else {
+       await handle(req, res, parsedUrl);
+     }
+   } catch (err) {
+     console.error("Error occurred handling", req.url, err);
+     res.statusCode = 500;
+     res.end("internal server error");
+   }
+ })
+   .once("error", (err: any) => {
+     console.error(err);
+     process.exit(1);
+   })
+   .listen(port, () => {
+     console.log(`> Ready on http://${hostname}:${port}`);
+   });
 })
